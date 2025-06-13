180e0e060b2bc54913187c90def85af547db3f93
Shubham Kamble <shubhamkamble3011@gmail.com>
2025-06-13 22:31:19 +0530

    add

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+function OpenCards() {
+    var AllElem = document.querySelectorAll('.elem');
+    var fullElemPage = document.querySelectorAll('.fullElem');
+    var AllFullElemBackBtn = document.querySelectorAll('.fullElem .back');
+
+    AllElem.forEach(function (elem) {
+        elem.addEventListener('click', function () {
+            fullElemPage[elem.id].style.display = 'block'
+        })
+    })
+    AllFullElemBackBtn.forEach(function (back) {
+        back.addEventListener('click', function () {
+            fullElemPage[back.id].style.display = 'none'
+        })
+    })
+}
+OpenCards();
