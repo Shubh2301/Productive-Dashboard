4aee40b0e25f40c70a5388cdb1fa500fc21d55d9
Shubham Kamble <shubhamkamble3011@gmail.com>
2025-06-14 22:51:23 +0530

    JS

--- script.js
+++ script.js
@@ -15,3 +15,62 @@ function OpenCards() {
     })
 }
 OpenCards();
+
+
+
+var currentTask = [];
+
+if (localStorage.getItem('currentTask')) {
+    currentTask = JSON.parse((localStorage.getItem('currentTask')))
+} else {
+    // localStorage.setItem('currentTask', currentTask)
+    console.log("empty");
+
+}
+
+
+
+function RenderTask() {
+    let allTask = document.querySelector('.allTask');
+    let sum = ''
+
+    currentTask.forEach( (elem)=> {
+        sum += `<div class="task">
+                        <h5>${elem.task} <span class=${elem.imp}>Imp</span></h5>
+                        <button>Mark As Completed </button>
+                    </div>`
+    })
+
+    allTask.innerHTML = sum;
+
+}
+
+RenderTask();
+
+
+let form = document.querySelector('.addTask form');
+let taskInput = document.querySelector(' .addTask form #input-task');
+let taskDetailsInput = document.querySelector(' .addTask form textarea');
+let taskCheckBox = document.querySelector(' .addTask form #check');
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    // console.log(taskInput.value);
+    // console.log(taskDetailsInput.value);
+    // console.log(taskCheckBox.value); 
+
+    currentTask.push({
+        task: taskInput.value,
+        details: taskDetailsInput.value,
+        imp: taskCheckBox.checked
+    })
+    localStorage.setItem('currentTask', JSON.stringify(currentTask))
+    taskInput.value = ''
+    taskDetailsInput.value = ''
+    taskCheckBox.checked = ''
+
+    RenderTask();
+
+})
+
+localStorage.clear()
